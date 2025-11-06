81f874496d554b29552af8ffcd3a367600e8c4b2
Mikołaj Świątek <mail@mikolajswiatek.com>
2025-11-06 18:41:45 +0100

    Fix shell reloading

--- .buildkite/hooks/pre-command.ps1
+++ .buildkite/hooks/pre-command.ps1
@@ -1,4 +1,5 @@
 # Shorten BUILDKITE_MESSAGE if needed to avoid filling the Windows env var buffer
 $env:BUILDKITE_MESSAGE = $env:BUILDKITE_MESSAGE.Substring(0, [System.Math]::Min(2048, $env:BUILDKITE_MESSAGE.Length))
 choco install awscli jq -y
+Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
 refreshenv
